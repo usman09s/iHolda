803d411489e08bf054bb8259669d4199ff47d86e
Burhan YILMAZ <burhan.yilmaaz@gmail.com>
2023-09-01 15:08:05 +0300

    feat: created folder structure

---
+++ src/modules/auth/components/TestComponents.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const TestComponent = () => {
+    return (
+        <View>
+            <Text>Test Component</Text>
+        </View>
+    )
+};
+
+export default TestComponent;
---
+++ src/modules/plastic/screens/PlasticScreen.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const PlasticScreen = () => {
+  return (
+    <View>
+      <Text>Plastic Screen</Text>
+    </View>
+  );
+};
+
+export default PlasticScreen;
