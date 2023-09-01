6dc1dfb09460277c4b383cb9b3b3f349e9164624
Burhan YILMAZ <burhan.yilmaaz@gmail.com>
2023-09-01 16:42:23 +0300

    feat: changed some assets

--- App.tsx
+++ App.tsx
@@ -1,7 +1,15 @@
-import { Text, View } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
 import MainNavigator from 'navigators/MainNavigator';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hideAsync();
+    }, 1000);
+  }, []);
+
   return <MainNavigator />;
 }
