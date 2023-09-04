01d21ab33d7d85bd0710fe1cccafc43a703d74d0
Burhan YILMAZ <burhan.yilmaaz@gmail.com>
2023-09-04 22:12:21 +0300

    feat: implemented referral code succesful screen

--- src/modules/auth/screens/ReferralCodeSuccessfulScreen.tsx
+++ src/modules/auth/screens/ReferralCodeSuccessfulScreen.tsx
@@ -1,12 +1,58 @@
-import { Text, View } from 'react-native';
+import { Image, ScrollView, Text, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import Button from 'components/Button';
+import { useSelector } from 'react-redux';
+import { userImageSelector } from 'store/auth/userSelectors';
+import { text } from 'theme/text';
 
 const ReferralCodeSuccessfulScreen = () => {
   const {} = useNavigation();
+  const userImage = useSelector(userImageSelector);
 
   return (
-    <View>
-      <Text>ReferralCodeSuccessfulScreen</Text>
+    <View className="flex-1 bg-blue justify-evenly px-7">
+      <ScrollView className="flex-1" contentContainerStyle={{ flex: 1 }}>
+        <View className="flex-1 justify-evenly ">
+          <View>
+            <Text className={text({ type: 'm48', class: 'text-center text-white-o-70 mb-8' })}>
+              All Done!
+            </Text>
+            <View className="flex-row self-center mb-20">
+              <View className="overflow-hidden border-white rounded-3xl border-4  -rotate-30 ">
+                <Image source={{ uri: userImage }} className="w-28 h-28" />
+              </View>
+              <View className="overflow-hidden border-white  rounded-3xl border-4   -left-8 top-2 rotate-30">
+                <Image source={{ uri: 'https://i.pravatar.cc/150?img=33' }} className="w-28 h-28" />
+              </View>
+            </View>
+          </View>
+          <View className="mb-5 bg-slate-300 py-5 px-2 rounded-2xl">
+            <Text className={text({ class: 'text-black-o-80 text-center', type: 'r12' })}>
+              @naturesbreath invited you so you both will receive
+            </Text>
+            <View className="mt-7 flex-row items-center">
+              <View className="overflow-hidden rounded-full border-4 border-white">
+                <Image
+                  source={{ uri: 'https://i.pravatar.cc/150?img=33' }}
+                  className="w-14 h-14 "
+                />
+              </View>
+              <View className="px-7 py-2 rounded-full bg-yellow-300 ml-4">
+                <Text className={text({ type: 'b2o' })}>500cfa</Text>
+              </View>
+            </View>
+            <View className="mb-5 flex-row items-center justify-end">
+              <View className="px-10 py-2 rounded-full bg-yellow-300 mr-4">
+                <Text className={text({ type: 'b2o' })}>500cfa</Text>
+              </View>
+              <View className="overflow-hidden rounded-full border-4 border-white">
+                <Image source={{ uri: userImage }} className="w-14 h-14" />
+              </View>
+            </View>
+          </View>
+          <Button title="Close" customContainer="self-center" type="borderedSolid" />
+        </View>
+      </ScrollView>
     </View>
   );
 };
