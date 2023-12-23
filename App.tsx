d0d790e820b9f8ebd6e72a9fb763581032d8a65e
Unknown-Coder769 <unknowncoder769@gmail.com>
2023-12-24 02:41:00 +0500

    feat: Connection Error Modal added

--- App.tsx
+++ App.tsx
@@ -1,18 +1,31 @@
 import 'react-native-gesture-handler';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import * as SplashScreen from 'expo-splash-screen';
 import MainNavigator from 'navigators/MainNavigator';
 import { useLoadFonts } from 'hooks/useLoadFonts';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Provider } from 'react-redux';
 import { store } from 'store/store';
+import NetInfo from '@react-native-community/netinfo';
 import Toast from 'react-native-toast-message';
+import CustomErrorModal from 'components/ErrorModal/errorModal';
 
 SplashScreen.preventAutoHideAsync();
 const queryClient = new QueryClient();
 
 export default function App() {
   const { loaded } = useLoadFonts();
+  const [networkErrorVisible, setNetworkErrorVisible] = useState(false);
+
+  const checkNetworkStatus = async () => {
+    try {
+      const state = await NetInfo.fetch();
+      console.log('Network Status Checked:', state.isConnected);
+      setNetworkErrorVisible(!state.isConnected);
+    } catch (error) {
+      console.error('Failed to check network status:', error);
+    }
+  };
 
   useEffect(() => {
     if (loaded) {
@@ -22,6 +35,15 @@ export default function App() {
     }
   }, [loaded]);
 
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener(state => {
+      console.log('Network Status Changed:', state.isConnected);
+      setNetworkErrorVisible(!state.isConnected);
+    });
+    checkNetworkStatus();
+    return unsubscribe;
+  }, []);
+
   if (!loaded) {
     return null;
   }
@@ -29,6 +51,12 @@ export default function App() {
   return (
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
+        <CustomErrorModal
+          errorText={'Please check your internet connection and try again.'}
+          visible={networkErrorVisible}
+          onClose={() => checkNetworkStatus()}
+          buttonTitle="Try Again"
+        />
         <MainNavigator />
       </QueryClientProvider>
       <Toast />
